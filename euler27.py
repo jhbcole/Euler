4c336230405da51b9a3d35245bb9b3bc8a326e3a
John Cole <jhbcole@gmail.com>
2013-06-12 17:08:44 -0500

    solved 27

--- euler27.py
+++ euler27.py
@@ -1,7 +1,23 @@
 import math
 
 def is_prime(n):
-  for i in xrange(1,int(math.sqrt(n))+1):
+  if n < 0:
+    return False
+  for i in xrange(2,int(math.sqrt(n))+1):
     if(n % i == 0):
       return False
   return True
+
+def main():
+  n = 0
+  maxl = (0, 0, 0) #(max consec, a, b)
+  for a in xrange(-1000,1000):
+    for b in xrange(-1000, 1000):
+      while is_prime(n**2 + a*n + b):
+        n += 1
+      if(n > maxl[0]):
+        maxl = (n, a, b)
+      n = 0
+  return maxl
+
+print main()
