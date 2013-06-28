ec64159d0f1e76c4a17ff8916889f8a4d15f32b3
John Cole <jhbcole@gmail.com>
2013-06-28 08:49:48 -0500

    got 433 to work, but still slow

--- euler433.c
+++ euler433.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 #define N 5000000
-#define N2 10
+#define N2 50
 
 pthread_mutex_t lock;
 unsigned long long* sum; 
@@ -23,7 +23,8 @@ void sigint_handler(int sig){
 void* euclid_steps(void* xp){
   int x = *(int *)xp;
   //pthread_detach(pthread_self());
-  int y,s = 0,tot = 0;
+  int y,s = 0;
+  unsigned long long tot = 0;
   for(y = 1; y<= N; y++){
     int ytmp = y;
     int xtmp = x;
@@ -35,7 +36,6 @@ void* euclid_steps(void* xp){
     }
     tot += (unsigned long long)s;
   }
-  printf("%lld\n", tot);
   pthread_mutex_lock(&lock);
   sum[(*(int *)xp)] = tot;
   pthread_mutex_unlock(&lock);
@@ -60,19 +60,9 @@ unsigned long long s(int n){
     }
   }
   unsigned long long tot = 0;
-  for(i = 0; i < n; i++){
-    if(sum[i] < (unsigned long long)0){
-      printf("BAD SUM: %d, %llu\n",i, sum[i]);
-      exit(0);
-    }
-    if(tot + sum[i] < (unsigned long long)0){
-      printf("OVERFLOW\n");
-      exit(0);
-    }
-    else{
-      tot += sum[i];
-      printf("\t %lld \t %lld\n", sum[i], tot);
-    }
+  for(i = 0; i < n; i++){ 
+    tot += (unsigned long long)sum[i];
+    //printf("\t %llu \t %llu\n", sum[i], tot);
   }
   return tot;
 }
@@ -90,7 +80,7 @@ int main(){
   }
 
   unsigned long long total = s(N2);
-  printf("total: %lld\n", total);
+  printf("total: %llu\n", total);
   pthread_mutex_destroy(&lock);
   free(sum);
   return 0;
